dcea8b787b4a80c89150b42fb593625b071a5a08
Fahad Hussain <sfahadh14@gmail.com>
2020-07-09 23:05:02 -0400

    added addEdge function in Starter folder

--- Undirected/Starter/Graph.js
+++ Undirected/Starter/Graph.js
@@ -6,4 +6,9 @@ Graph.prototype.addVertix = function(vertix) {
     this.adjacencyList[vertix] = [];
 }
 
+Graph.prototype.addEdge = function(vertix1, vertix2) {
+    this.adjacencyList[vertix1] = vertix2;
+    this.adjacencyList[vertix2] = vertix1;
+}
+
 module.exports = Graph;
