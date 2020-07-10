a2d227031da4b105e32975b497e75b97133bb997
Fahad Hussain <sfahadh14@gmail.com>
2020-07-09 23:12:05 -0400

    added addVertix and addEdge functions for directed folder

--- Starter/Directed/Graph.js
+++ Starter/Directed/Graph.js
@@ -2,4 +2,12 @@ function Graph() {
     this.adjacencyList = {};
 }
 
+Graph.prototype.addVertix = function(vertix) {
+    this.adjacencyList[vertix] = [];
+}
+
+Graph.prototype.addEdge = function(vertix1, vertix2) {
+    this.adjacencyList[vertix1] = vertix2;
+}
+
 module.exports = Graph;
