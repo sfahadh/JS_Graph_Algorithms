a94524a9da4ed891f817e3f2834c396992a47bab
Fahad Hussain <sfahadh14@gmail.com>
2020-07-09 23:41:19 -0400

    added addVertix problem for undirected graph

---
+++ UndirectedGraph/addVertix/problem.js
@@ -0,0 +1,21 @@
+// ? In Visual Studo Code, install "Better Comments" to see colorful effects of comments
+
+/*
+TODO: Complete the following add vertix problem below. There are two inputs, the adjacency list of the graph and the vertix. The goal of this problem is to add the vertix to the adjacency list and then return that list.
+
+function Graph() {
+    this.adjacencyList = {};
+}
+*/
+
+const addVertix = (adjacencyList, vertix) => {
+    
+}
+
+module.exports = addVertix;
+
+/*
+! Test Case:
+* Sample Input: { "A": ["B", "C"], "B": ["A"], "C": ["A"] }, "D"
+* Sample Output: { "A": ["B", "C"], "B": ["A"], "C": ["A"], "D": [] }
+*/
