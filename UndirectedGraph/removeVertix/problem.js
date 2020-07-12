30d25553b02ba75954a5c6b78abf800440fdfdaa
Fahad Hussain <sfahadh14@gmail.com>
2020-07-12 13:06:47 -0400

    added removeVertix problem in undirected graph folder

---
+++ UndirectedGraph/removeVertix/problem.js
@@ -0,0 +1,21 @@
+// ? In Visual Studo Code, install "Better Comments" to see colorful effects of comments
+
+/*
+TODO: Complete the following remove vertix problem below. There are two inputs, the adjacency list of the graph and the vertix that is to be removed from the adjacency list. The goal of this problem is to completely remove the vertix including all its relationships with other vertices and then return the adjacency list.
+
+function Graph() {
+    this.adjacencyList = {};
+}
+*/
+
+const removeVertix = (adjacencyList, vertix) => {
+
+}
+
+module.exports = removeVertix;
+
+/*
+! Test Case:
+* Sample Input: { "A": [], "B": ["C"], "C": ["B", "A"] }, "B"
+* Sample Output: { "A": [], "C": ["A"] }
+*/
