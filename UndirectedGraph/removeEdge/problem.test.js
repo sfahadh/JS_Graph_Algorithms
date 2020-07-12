f2dbd426d8fa0672f16c3df7cfbf1897f68cbcba
Fahad Hussain <sfahadh14@gmail.com>
2020-07-12 12:42:18 -0400

    added more test cases for removeEdge problem in undirected graph folder

--- UndirectedGraph/removeEdge/problem.test.js
+++ UndirectedGraph/removeEdge/problem.test.js
@@ -12,8 +12,63 @@ test("remove the edge between vertices 'B' and 'C'", () => {
     generateVertices(outputGraph, arr);
 
     inputGraph.addEdge("B", "C")
-    console.log(inputGraph);
-    console.log(outputGraph);
 
     expect(removeEdge(inputGraph.adjacencyList, "B", "C")).toEqual(outputGraph.adjacencyList);
+})
+
+// Test Case 2:
+test("remove the edge between vertices 'ying' and 'yang'", () => {
+    const inputGraph = new Graph();
+    const outputGraph = new Graph();
+    const arr = ["ying", "yang"];
+
+    generateVertices(inputGraph, arr);
+    generateVertices(outputGraph, arr);
+
+    inputGraph.addEdge("ying", "yang");
+
+    expect(removeEdge(inputGraph.adjacencyList, "ying", "yang")).toEqual(outputGraph.adjacencyList);
+})
+
+// Test Case 3:
+test("remove the edge between vertices 'loki' and 'yang'", () => {
+    const inputGraph = new Graph();
+    const outputGraph = new Graph();
+    const arr = ["hulk", "loki", "thor", "thanos"];
+
+    generateVertices(inputGraph, arr);
+    generateVertices(outputGraph, arr);
+
+    inputGraph.addEdge("loki", "thor");
+    inputGraph.addEdge("hulk", "thor");
+
+    expect(removeEdge(inputGraph.adjacencyList, "loki", "thor")).toEqual(outputGraph.adjacencyList);
+})
+
+// Test Case 4:
+test("remove the edge between vertices 'left' and 'right'", () => {
+    const inputGraph = new Graph();
+    const outputGraph = new Graph();
+    const arr = ["left", "right", "up", "down"];
+
+    generateVertices(inputGraph, arr);
+    generateVertices(outputGraph, arr);
+
+    inputGraph.addEdge("left", "right");
+    inputGraph.addEdge("up", "down");
+
+    expect(removeEdge(inputGraph.adjacencyList, "left", "right")).toEqual(outputGraph.adjacencyList);
+})
+
+// Test Case 5:
+test("remove the edge between the vertix 'myself' with itself", () => {
+    const inputGraph = new Graph();
+    const outputGraph = new Graph();
+
+    inputGraph.addVertix("myself");
+    outputGraph.addVertix("myself")
+
+    inputGraph.addEdge("myself", "myself");
+
+    expect(removeEdge(inputGraph.adjacencyList, "myself", "myself")).toEqual(outputGraph.adjacencyList);
 })
