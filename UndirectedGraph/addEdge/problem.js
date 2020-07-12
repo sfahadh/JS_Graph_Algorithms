596da0099b6152b28c244b498cb35672b3f516b7
Fahad Hussain <sfahadh14@gmail.com>
2020-07-11 21:12:13 -0400

    added removeEdge problem in undirected graph folder

--- UndirectedGraph/addEdge/problem.js
+++ UndirectedGraph/addEdge/problem.js
@@ -9,9 +9,7 @@ function Graph() {
 */
 
 const addEdge = (adjacencyList, vertix1, vertix2) => {
-    adjacencyList[vertix1].push(vertix2);
-    adjacencyList[vertix2].push(vertix1);
-    return adjacencyList;
+
 }
 
 module.exports = addEdge;
---
+++ UndirectedGraph/removeEdge/problem.js
@@ -0,0 +1,21 @@
+// ? In Visual Studo Code, install "Better Comments" to see colorful effects of comments
+
+/*
+TODO: Complete the following remove edge problem below. There are three inputs, the adjacency list of the graph and the 2 vertices to remove the relationship. The goal of this problem is to remove an edge between the two vertices then return the adjacency list.
+
+function Graph() {
+    this.adjacencyList = {};
+}
+*/
+
+const removeEdge = (adjacencyList, vertix1, vertix2) => {
+
+}
+
+module.exports = removeEdge;
+
+/*
+! Test Case:
+* Sample Input: { "A": [], "B": ["C"], "C": ["B"] }, "B", "C"
+* Sample Output: { "A": [], "B": [], "C": [] }
+*/
