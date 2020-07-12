9d24d5fa0a54867e6d31a198f58bdd071ecb04e1
Fahad Hussain <sfahadh14@gmail.com>
2020-07-11 20:34:37 -0400

    added more test cases for addEdge problem in undirected graph folder

--- UndirectedGraph/addEdge/problem.test.js
+++ UndirectedGraph/addEdge/problem.test.js
@@ -14,3 +14,54 @@ test("established an edge between vertices 'B' and 'C'", () => {
 
     expect(addEdge(inputGraph.adjacencyList, "B", "C")).toEqual(outputGraph.adjacencyList);
 })
+
+// Test Case 2:
+test("established an edge between vertices 'ying' and 'yang'", () => {
+    const inputGraph = new Graph();
+    const outputGraph = new Graph();
+    const arr = ["ying", "yang"];
+
+    generateVertices(inputGraph, arr);
+    generateVertices(outputGraph, arr);
+    outputGraph.addEdge("ying", "yang");
+
+    expect(addEdge(inputGraph.adjacencyList, "ying", "yang")).toEqual(outputGraph.adjacencyList);
+})
+
+// Test Case 3:
+test("established an edge between vertices 'loki' and 'yang'", () => {
+    const inputGraph = new Graph();
+    const outputGraph = new Graph();
+    const arr = ["hulk", "loki", "thor", "thanos"];
+
+    generateVertices(inputGraph, arr);
+    generateVertices(outputGraph, arr);
+    outputGraph.addEdge("loki", "thor");
+
+    expect(addEdge(inputGraph.adjacencyList, "loki", "thor")).toEqual(outputGraph.adjacencyList);
+})
+
+// Test Case 4:
+test("established an edge between vertices 'left' and 'right'", () => {
+    const inputGraph = new Graph();
+    const outputGraph = new Graph();
+    const arr = ["left", "right", "up", "down"];
+
+    generateVertices(inputGraph, arr);
+    generateVertices(outputGraph, arr);
+    outputGraph.addEdge("left", "right");
+
+    expect(addEdge(inputGraph.adjacencyList, "left", "right")).toEqual(outputGraph.adjacencyList);
+})
+
+// Test Case 5:
+test("established an edge between the vertix 'myself' with itself", () => {
+    const inputGraph = new Graph();
+    const outputGraph = new Graph();
+
+    inputGraph.addVertix("myself");
+    outputGraph.addVertix("myself")
+    outputGraph.addEdge("myself", "myself");
+
+    expect(addEdge(inputGraph.adjacencyList, "myself", "myself")).toEqual(outputGraph.adjacencyList);
+})
