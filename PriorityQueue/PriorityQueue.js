a9f8bf63319f5ef2638fe798d821831aecb79519
Fahad Hussain <sfahadh14@gmail.com>
2020-07-18 18:15:18 -0400

    moved enqueue function to its own file for priority queue

--- PriorityQueue/PriorityQueue.js
+++ PriorityQueue/PriorityQueue.js
@@ -1,30 +1,5 @@
-const Node = require("./Node");
-
 function PriorityQueue() {
     this.values = [];
-}
-
-PriorityQueue.prototype.enqueue = function (data, priority) {
-    const node = new Node(data, priority);
-    this.values.push(node);
-    this.bubbleUp();
-}
-
-PriorityQueue.prototype.bubbleUp = function () {
-    let index = this.values.length - 1;
-    const element = this.values[index];
-    let parentIndex;
-    let parent;
-
-    while (index > 0) {
-        parentIndex = Math.floor((index - 1) / 2);
-        parent = this.values[parentIndex];
-        if (element.priority <= parent.priority) break;
-
-        this.values[parentIndex] = element;
-        this.values[index] = parent;
-        index = parentIndex;
-    }
 }
 
 module.exports = PriorityQueue;
