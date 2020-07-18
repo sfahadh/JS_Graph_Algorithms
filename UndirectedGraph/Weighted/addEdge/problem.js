d429a834185219af2cef0a4e4bd80c187e0c4dc5
Fahad Hussain <sfahadh14@gmail.com>
2020-07-18 19:44:42 -0400

    added more test cases for addEdge problem in undirected weighted graph folder

--- UndirectedGraph/Weighted/addEdge/problem.js
+++ UndirectedGraph/Weighted/addEdge/problem.js
@@ -9,7 +9,9 @@ function UnweightedGraph() {
 */
 
 const addEdge = (adjacencyList, vertix1, vertix2, weight) => {
-
+    adjacencyList[vertix1].push({ "vertix": vertix2, weight });
+    adjacencyList[vertix2].push({ "vertix": vertix1, weight });
+    return adjacencyList;
 }
 
 module.exports = addEdge;
--- UndirectedGraph/Weighted/addEdge/problem.test.js
+++ UndirectedGraph/Weighted/addEdge/problem.test.js
@@ -14,3 +14,54 @@ test("established an edge between vertices 'B' and 'C'", () => {
 
     expect(addEdge(inputGraph.adjacencyList, "B", "C", 10)).toEqual(outputGraph.adjacencyList);
 })
+
+// Test Case 2:
+test("established an edge between vertices 'ying' and 'yang'", () => {
+    const inputGraph = new Graph();
+    const outputGraph = new Graph();
+    const arr = ["ying", "yang"];
+
+    generateVertices(inputGraph, arr);
+    generateVertices(outputGraph, arr);
+    outputGraph.addEdge("ying", "yang", 69);
+
+    expect(addEdge(inputGraph.adjacencyList, "ying", "yang", 69)).toEqual(outputGraph.adjacencyList);
+})
+
+// Test Case 3:
+test("established an edge between vertices 'loki' and 'yang'", () => {
+    const inputGraph = new Graph();
+    const outputGraph = new Graph();
+    const arr = ["hulk", "loki", "thor", "thanos"];
+
+    generateVertices(inputGraph, arr);
+    generateVertices(outputGraph, arr);
+    outputGraph.addEdge("loki", "thor", 5);
+
+    expect(addEdge(inputGraph.adjacencyList, "loki", "thor", 5)).toEqual(outputGraph.adjacencyList);
+})
+
+// Test Case 4:
+test("established an edge between vertices 'left' and 'right'", () => {
+    const inputGraph = new Graph();
+    const outputGraph = new Graph();
+    const arr = ["left", "right", "up", "down"];
+
+    generateVertices(inputGraph, arr);
+    generateVertices(outputGraph, arr);
+    outputGraph.addEdge("left", "right", 100);
+
+    expect(addEdge(inputGraph.adjacencyList, "left", "right", 100)).toEqual(outputGraph.adjacencyList);
+})
+
+// Test Case 5:
+test("established an edge between the vertix 'myself' with itself", () => {
+    const inputGraph = new Graph();
+    const outputGraph = new Graph();
+
+    inputGraph.addVertix("myself");
+    outputGraph.addVertix("myself")
+    outputGraph.addEdge("myself", "myself", 0);
+
+    expect(addEdge(inputGraph.adjacencyList, "myself", "myself", 0)).toEqual(outputGraph.adjacencyList);
+})
