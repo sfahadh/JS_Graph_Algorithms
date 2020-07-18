cb89aa0dce26eb8d1211a01bccd41dfa2167e8af
Fahad Hussain <sfahadh14@gmail.com>
2020-07-17 21:38:28 -0400

    added weightedgraph starter function

---
+++ Starter/Undirected/WeightedGraph.js
@@ -0,0 +1,14 @@
+function WeightedGraph() {
+    this.adjacencyList = {};
+}
+
+WeightedGraph.prototype.addVertix = function(vertix) {
+    this.adjacencyList[vertix] = [];
+}
+
+WeightedGraph.prototype.addEdge = function(vertix1, vertix2, weight) {
+    this.adjacencyList[vertix1].push({ vertix: vertix2, weight });
+    this.adjacencyList[vertix2].push({ vertix: vertix1, weight });
+}
+
+module.exports = WeightedGraph;
