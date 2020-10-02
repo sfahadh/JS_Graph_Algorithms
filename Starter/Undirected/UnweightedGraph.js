3b5b8e4cf1b349693f7d869d908e51550dcc388d
sfahadh <sfahadh14@gmail.com>
2020-10-02 13:54:22 -0400

    added structure folder with 4 sub folders within it

--- Starter/Undirected/UnweightedGraph.js
+++ Starter/Undirected/UnweightedGraph.js
@@ -2,11 +2,11 @@ function UnweightedGraph() {
     this.adjacencyList = {};
 }
 
-UnweightedGraph.prototype.addVertix = function(vertix) {
+UnweightedGraph.prototype.addVertix = function (vertix) {
     this.adjacencyList[vertix] = [];
 }
 
-UnweightedGraph.prototype.addEdge = function(vertix1, vertix2) {
+UnweightedGraph.prototype.addEdge = function (vertix1, vertix2) {
     this.adjacencyList[vertix1].push(vertix2);
     this.adjacencyList[vertix2].push(vertix1);
 }
