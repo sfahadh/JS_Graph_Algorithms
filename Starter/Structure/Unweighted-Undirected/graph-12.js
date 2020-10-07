cf7a0623f320d9d1b33b30079ca2a486eced5131
sfahadh <sfahadh14@gmail.com>
2020-10-07 10:28:54 -0400

    added graph-12 structure file under unweighted undirected structure folder

---
+++ Starter/Structure/Unweighted-Undirected/graph-12.js
@@ -0,0 +1,29 @@
+const Graph = require("../../Undirected/UnweightedGraph");
+const generateVertices = require("../../generateVertices");
+
+const vertices = ["-2", "-1", "0", "-3", "-5", "-4"];
+
+const constructGraph = () => {
+    const graph = new Graph;
+    generateVertices(graph, vertices);
+
+    graph.addEdge("-5", "0");
+    graph.addEdge("-5", "-1");
+    graph.addEdge("-5", "-2");
+    graph.addEdge("-5", "-3");
+    graph.addEdge("-5", "-4");
+    graph.addEdge("-4", "0");
+    graph.addEdge("-4", "-1");
+    graph.addEdge("-4", "-2");
+    graph.addEdge("-4", "-3");
+    graph.addEdge("-3", "0");
+    graph.addEdge("-3", "-1");
+    graph.addEdge("-3", "-2");
+    graph.addEdge("-2", "0");
+    graph.addEdge("-2", "-1");
+    graph.addEdge("-1", "0");
+
+    return graph;
+}
+
+module.exports = constructGraph;
