4ce2c02e8822567144dde259251b980192ad9e74
sfahadh <sfahadh14@gmail.com>
2020-10-07 10:42:18 -0400

    added graph-3 structure file under unweighted directed structure folder

---
+++ Starter/Structure/Unweighted-Directed/graph-3.js
@@ -0,0 +1,17 @@
+const Graph = require("../../Directed/UnweightedGraph");
+const generateVertices = require("../../generateVertices");
+
+const vertices = ["-1", "-2", "-3"];
+
+const constructGraph = () => {
+    const graph = new Graph;
+    generateVertices(graph, vertices);
+
+    graph.addEdge("-1", "-3");
+    graph.addEdge("-2", "-1");
+    graph.addEdge("-3", "-2");
+
+    return graph;
+}
+
+module.exports = constructGraph;
