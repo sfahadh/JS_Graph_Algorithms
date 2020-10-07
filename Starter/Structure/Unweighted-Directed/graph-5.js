1a83d71749d59aba0a68eeac7bc57e3fc4854b58
sfahadh <sfahadh14@gmail.com>
2020-10-07 10:46:35 -0400

    added graph-5 structure file under unweighted directed structure folder

---
+++ Starter/Structure/Unweighted-Directed/graph-5.js
@@ -0,0 +1,18 @@
+const Graph = require("../../Directed/UnweightedGraph");
+const generateVertices = require("../../generateVertices");
+
+const vertices = ["6", "-6", "8", "7", "-7"];
+
+const constructGraph = () => {
+    const graph = new Graph;
+    generateVertices(graph, vertices);
+
+    graph.addEdge("6", "-6");
+    graph.addEdge("8", "-6");
+    graph.addEdge("8", "7");
+    graph.addEdge("-7", "7");
+
+    return graph;
+}
+
+module.exports = constructGraph;
