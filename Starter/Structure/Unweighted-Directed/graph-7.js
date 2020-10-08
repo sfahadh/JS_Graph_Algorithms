2d4a0c496c08993e34db32fb2fc5a06fa52982f9
sfahadh <sfahadh14@gmail.com>
2020-10-08 19:10:10 -0400

    added graph-7 structure file under unweighted directed structure folder

---
+++ Starter/Structure/Unweighted-Directed/graph-7.js
@@ -0,0 +1,26 @@
+const Graph = require("../../Directed/UnweightedGraph");
+const generateVertices = require("../../generateVertices");
+
+const vertices = [1, 2, 7, 3, 5, 4, 6];
+
+const constructGraph = () => {
+    const graph = new Graph;
+    generateVertices(graph, vertices);
+
+    graph.addEdge(1, 7);
+    graph.addEdge(1, 3);
+    graph.addEdge(1, 2);
+    graph.addEdge(2, 3);
+    graph.addEdge(2, 5);
+    graph.addEdge(7, 4);
+    graph.addEdge(3, 7);
+    graph.addEdge(3, 4);
+    graph.addEdge(3, 6);
+    graph.addEdge(5, 3);
+    graph.addEdge(5, 6);
+    graph.addEdge(6, 4);
+
+    return graph;
+}
+
+module.exports = constructGraph;
