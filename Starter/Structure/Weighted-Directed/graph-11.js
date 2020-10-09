bbca3a291f4981c3f3b836d1e5266e8c8a85b149
sfahadh <sfahadh14@gmail.com>
2020-10-09 13:46:16 -0400

    added 11th graph structure file under weighted directed folder

---
+++ Starter/Structure/Weighted-Directed/graph-11.js
@@ -0,0 +1,28 @@
+const Graph = require("../../Directed/UnweightedGraph");
+const generateVertices = require("../../generateVertices");
+
+const vertices = ["A", "B", "C", "D", "E", "F", "G", "H"];
+
+const constructGraph = () => {
+    const graph = new Graph;
+    generateVertices(graph, vertices);
+
+    graph.addEdge("A", "B", 2);
+    graph.addEdge("A", "F", 7);
+    graph.addEdge("A", "E", 3);
+    graph.addEdge("B", "F", 6);
+    graph.addEdge("B", "C", 5);
+    graph.addEdge("B", "G", 2);
+    graph.addEdge("C", "D", 4);
+    graph.addEdge("C", "F", 3);
+    graph.addEdge("C", "G", 5);
+    graph.addEdge("D", "G", 3);
+    graph.addEdge("D", "H", 1);
+    graph.addEdge("E", "F", 6);
+    graph.addEdge("G", "F", 6);
+    graph.addEdge("G", "H", 5);
+
+    return graph;
+}
+
+module.exports = constructGraph;
