4d82b27da0557359f65133fdb8d74a2fcd88e4ff
sfahadh <sfahadh14@gmail.com>
2020-10-08 21:17:24 -0400

    added 5th graph structure file under weighted directed folder

---
+++ Starter/Structure/Weighted-Directed/graph-5.js
@@ -0,0 +1,20 @@
+const Graph = require("../../Directed/UnweightedGraph");
+const generateVertices = require("../../generateVertices");
+
+const vertices = [15, 2, 6, 3, 16, 9, 10];
+
+const constructGraph = () => {
+    const graph = new Graph;
+    generateVertices(graph, vertices);
+
+    graph.addEdge(15, 10, -7);
+    graph.addEdge(2, 10, 11);
+    graph.addEdge(6, 10, -9);
+    graph.addEdge(3, 10, 18);
+    graph.addEdge(16, 10, -1);
+    graph.addEdge(9, 10, 22);
+
+    return graph;
+}
+
+module.exports = constructGraph;
