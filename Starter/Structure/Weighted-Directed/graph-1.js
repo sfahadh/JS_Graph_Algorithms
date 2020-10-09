2275261678efe727dcfb97c83013d3b0bf1cc234
sfahadh <sfahadh14@gmail.com>
2020-10-08 20:15:09 -0400

    added 1st graph structure file under weighted directed folder

---
+++ Starter/Structure/Weighted-Directed/graph-1.js
@@ -0,0 +1,13 @@
+const Graph = require("../../Directed/UnweightedGraph");
+const generateVertices = require("../../generateVertices");
+
+const vertices = ["Yin", "Yang"];
+
+const constructGraph = () => {
+    const graph = new Graph;
+    generateVertices(graph, vertices);
+    graph.addEdge("Yin", "Yang", 69);
+    return graph;
+}
+
+module.exports = constructGraph;
