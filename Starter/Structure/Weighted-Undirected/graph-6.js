172936c4dda5d0f4350e3f97d40b8f736915c913
sfahadh <sfahadh14@gmail.com>
2020-10-09 17:29:21 -0400

    added 6th graph structure file under weighted undirected folder

---
+++ Starter/Structure/Weighted-Undirected/graph-6.js
@@ -0,0 +1,20 @@
+const Graph = require("../../Directed/UnweightedGraph");
+const generateVertices = require("../../generateVertices");
+
+const vertices = [47, 26, 91, 99, 19, 74, 62];
+
+const constructGraph = () => {
+    const graph = new Graph;
+    generateVertices(graph, vertices);
+
+    graph.addEdge(47, 26, 38);
+    graph.addEdge(47, 99, 44);
+    graph.addEdge(26, 91, 32);
+    graph.addEdge(99, 74, 45);
+    graph.addEdge(74, 62, 41);
+    graph.addEdge(62, 19, 36);
+
+    return graph;
+}
+
+module.exports = constructGraph;
