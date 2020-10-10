6363f1f4240d87ca2c81f269e3128b0390cfb89c
sfahadh <sfahadh14@gmail.com>
2020-10-09 21:59:34 -0400

    added 10th graph structure file under weighted undirected folder

---
+++ Starter/Structure/Weighted-Undirected/graph-10.js
@@ -0,0 +1,20 @@
+const Graph = require("../../Directed/UnweightedGraph");
+const generateVertices = require("../../generateVertices");
+
+const vertices = ["A", "B", "C", "D", "E"];
+
+const constructGraph = () => {
+    const graph = new Graph;
+    generateVertices(graph, vertices);
+
+    graph.addEdge("A", "B", 11);
+    graph.addEdge("A", "E", 13);
+    graph.addEdge("B", "C", 7);
+    graph.addEdge("B", "E", 8);
+    graph.addEdge("C", "D", 10);
+    graph.addEdge("D", "E", 9);
+
+    return graph;
+}
+
+module.exports = constructGraph;
