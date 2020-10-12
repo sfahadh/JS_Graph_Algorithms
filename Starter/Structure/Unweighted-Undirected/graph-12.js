ce38706f88ba69652f707ec84d6a4ddc56956b22
sfahadh <sfahadh14@gmail.com>
2020-10-12 11:14:26 -0400

    replaced string of numbers to actual numbers in graph-12 problem under unweighted undirected

--- Starter/Structure/Unweighted-Undirected/graph-12.js
+++ Starter/Structure/Unweighted-Undirected/graph-12.js
@@ -1,27 +1,27 @@
 const Graph = require("../../Undirected/UnweightedGraph");
 const generateVertices = require("../../generateVertices");
 
-const vertices = ["-2", "-1", "0", "-3", "-5", "-4"];
+const vertices = [-2, -1, 0, -3, -4, -5];
 
 const constructGraph = () => {
     const graph = new Graph;
     generateVertices(graph, vertices);
 
-    graph.addEdge("-5", "0");
-    graph.addEdge("-5", "-1");
-    graph.addEdge("-5", "-2");
-    graph.addEdge("-5", "-3");
-    graph.addEdge("-5", "-4");
-    graph.addEdge("-4", "0");
-    graph.addEdge("-4", "-1");
-    graph.addEdge("-4", "-2");
-    graph.addEdge("-4", "-3");
-    graph.addEdge("-3", "0");
-    graph.addEdge("-3", "-1");
-    graph.addEdge("-3", "-2");
-    graph.addEdge("-2", "0");
-    graph.addEdge("-2", "-1");
-    graph.addEdge("-1", "0");
+    graph.addEdge(-5, 0);
+    graph.addEdge(-5, -1);
+    graph.addEdge(-5, -2);
+    graph.addEdge(-5, -3);
+    graph.addEdge(-5, -4);
+    graph.addEdge(-4, 0);
+    graph.addEdge(-4, -1);
+    graph.addEdge(-4, -2);
+    graph.addEdge(-4, -3);
+    graph.addEdge(-3, 0);
+    graph.addEdge(-3, -1);
+    graph.addEdge(-3, -2);
+    graph.addEdge(-2, 0);
+    graph.addEdge(-2, -1);
+    graph.addEdge(-1, 0);
 
     return graph;
 }
