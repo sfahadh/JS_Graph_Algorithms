b245d81a4863d7fb092676710b05ce24769130aa
sfahadh <sfahadh14@gmail.com>
2020-10-12 11:17:18 -0400

    replaced string of numbers to actual numbers in graph-5 problem under unweighted directed

--- Starter/Structure/Unweighted-Directed/graph-5.js
+++ Starter/Structure/Unweighted-Directed/graph-5.js
@@ -1,16 +1,16 @@
 const Graph = require("../../Directed/UnweightedGraph");
 const generateVertices = require("../../generateVertices");
 
-const vertices = ["6", "-6", "8", "7", "-7"];
+const vertices = [6, -6, 8, 7, -7];
 
 const constructGraph = () => {
     const graph = new Graph;
     generateVertices(graph, vertices);
 
-    graph.addEdge("6", "-6");
-    graph.addEdge("8", "-6");
-    graph.addEdge("8", "7");
-    graph.addEdge("-7", "7");
+    graph.addEdge(6, -6);
+    graph.addEdge(8, - 6);
+    graph.addEdge(8, 7);
+    graph.addEdge(-7, 7);
 
     return graph;
 }
