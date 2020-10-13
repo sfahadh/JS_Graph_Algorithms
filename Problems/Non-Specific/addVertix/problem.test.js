9ecb322cf2c11d5feaa0082988002911fd3a1d6c
sfahadh <sfahadh14@gmail.com>
2020-10-13 14:58:17 -0400

    added 4 more test cases for add vertix problem

--- Problems/Non-Specific/addVertix/problem.test.js
+++ Problems/Non-Specific/addVertix/problem.test.js
@@ -1,12 +1,16 @@
 const addVertix = require("./problem");
 const UUG1 = require("../../../Starter/UnweightedUndirected/Structure/graph-1");
 const UUG5 = require("../../../Starter/UnweightedUndirected/Structure/graph-5");
+const UUG9 = require("../../../Starter/UnweightedUndirected/Structure/graph-9");
 const UDG2 = require("../../../Starter/UnweightedDirected/Structure/graph-2");
 const UDG6 = require("../../../Starter/UnweightedDirected/Structure/graph-7");
+const UDG10 = require("../../../Starter/UnweightedDirected/Structure/graph-10");
 const WDG3 = require("../../../Starter/WeightedDirected/Structure/graph-3");
 const WDG7 = require("../../../Starter/WeightedDirected/Structure/graph-7");
+const WDG11 = require("../../../Starter/WeightedDirected/Structure/graph-11");
 const WUG4 = require("../../../Starter/WeightedUndirected/Structure/graph-4");
 const WUG8 = require("../../../Starter/WeightedUndirected/Structure/graph-8");
+const WUG12 = require("../../../Starter/WeightedUndirected/Structure/graph-12");
 
 // Test Case 1:
 test("add the vertex H to the adjacency list for Unweighted Undirected graph", () => {
@@ -58,8 +62,36 @@ test("add the vertex 7 to the adjacency list for Weighted Directed graph", () =>
 })
 
 // Test Case 8:
-test("add the vertex 8 to the adjacency list for Weighted Directed graph", () => {
+test("add the vertex 8 to the adjacency list for Weighted Undirected graph", () => {
     const result = WUG8();
     result.addVertix(8);
     expect(addVertix(WUG8().adjacencyList, 8)).toEqual(result.adjacencyList);
+})
+
+// Test Case 9:
+test("add the vertex 'CT' to the adjacency list for Unweighted Undirected graph", () => {
+    const result = UUG9();
+    result.addVertix('CT');
+    expect(addVertix(UUG9().adjacencyList, 'CT')).toEqual(result.adjacencyList);
+})
+
+// Test Case 10:
+test("add the vertex 0 to the adjacency list for Unweighted Directed graph", () => {
+    const result = UDG10();
+    result.addVertix(0);
+    expect(addVertix(UDG10().adjacencyList, 0)).toEqual(result.adjacencyList);
+})
+
+// Test Case 11:
+test("add the vertex 'I' to the adjacency list for Weighted Directed graph", () => {
+    const result = WDG11();
+    result.addVertix('I');
+    expect(addVertix(WDG11().adjacencyList, 'I')).toEqual(result.adjacencyList);
+})
+
+// Test Case 12:
+test("add the vertex 'S' to the adjacency list for Weighted Undirected graph", () => {
+    const result = WUG12();
+    result.addVertix('S');
+    expect(addVertix(WUG12().adjacencyList, 'S')).toEqual(result.adjacencyList);
 })
