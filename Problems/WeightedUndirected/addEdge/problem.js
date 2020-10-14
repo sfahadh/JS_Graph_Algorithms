be56bd5934c1f781d453f43afb02eae4700e5a42
sfahadh <sfahadh14@gmail.com>
2020-10-14 07:40:22 -0400

    cleared test cases for add edge problem in weighted undirected

--- Problems/WeightedUndirected/addEdge/problem.js
+++ Problems/WeightedUndirected/addEdge/problem.js
@@ -12,7 +12,9 @@ function UnweightedGraph() {
 */
 
 const addEdge = (adjacencyList, vertix1, vertix2, weight) => {
-
+    adjacencyList[vertix1].push({ "vertix": vertix2, weight });
+    adjacencyList[vertix2].push({ "vertix": vertix1, weight });
+    return adjacencyList;
 }
 
 module.exports = addEdge;
