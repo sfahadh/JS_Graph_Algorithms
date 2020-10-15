2c5de466e37cb8668bd3de180c401b709c149562
sfahadh <sfahadh14@gmail.com>
2020-10-15 10:47:22 -0400

    added add adge problem in unweighted directed

---
+++ Problems/UnweightedDirected/addEdge/problem.js
@@ -0,0 +1,28 @@
+// ? In Visual Studo Code, install "Better Comments" to see colorful effects of comments
+
+/*
+TODO: Complete the following add edge problem below. There are four inputs, the adjacency list 
+TODO: of the graph, the 2 vertices to establish the relationship, and the weight of the 
+TODO: relationship between the 2 vertices. The goal of this problem is to add an edge with weight 
+TODO: between the two vertices then return the adjacency list.
+
+function UnweightedDirectedGraph() {
+    this.adjacencyList = {};
+}
+*/
+
+const addEdge = (adjacencyList, vertix1, vertix2) => {
+
+}
+
+module.exports = addEdge;
+
+/*
+! Test Case:
+* Sample Input: { "A": [], "B": [], "C": [] }, "B", "C"
+* Sample Output: {
+    "A": [],
+    "B": ["C"],
+    "C": []
+}
+*/
