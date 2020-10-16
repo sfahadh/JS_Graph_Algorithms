5baa6e66ecd03364c91c7cd3bb8dc9991a12f703
sfahadh <sfahadh14@gmail.com>
2020-10-16 14:39:57 -0400

    added 4 test cases for remove edge problem in unweighted undirected

--- Problems/UnweightedUndirected/removeEdge/problem.js
+++ Problems/UnweightedUndirected/removeEdge/problem.js
@@ -13,7 +13,6 @@ function UnweightedGraph() {
 const removeEdge = (adjacencyList, vertix1, vertix2) => {
     adjacencyList[vertix1] = adjacencyList[vertix1].filter(v => v !== vertix2);
     adjacencyList[vertix2] = adjacencyList[vertix2].filter(v => v !== vertix1);
-    console.log(adjacencyList);
     return adjacencyList;
 }
 
--- Problems/UnweightedUndirected/removeEdge/problem.test.js
+++ Problems/UnweightedUndirected/removeEdge/problem.test.js
@@ -29,3 +29,61 @@ test("remove the edge between vertices 'D' and 'E'", () => {
 
     expect(removeEdge(Graph1().adjacencyList, "D", "E")).toEqual(graph.adjacencyList);
 })
+
+// Test Case 2: //? refer to Starter -> UnweightedUndirected -> Images -> graph-2.png
+test("remove the edge between vertices 'FB' and 'MS'", () => {
+    const vertices = ["Apple", "Netflix", "Google", "Amazon", "MS", "FB"];
+    const graph = new Graph();
+
+    generateVertices(graph, vertices);
+    graph.addEdge("Apple", "Netflix");
+    graph.addEdge("Apple", "Google");
+    graph.addEdge("Netflix", "Amazon");
+    graph.addEdge("Netflix", "FB");
+    graph.addEdge("Google", "FB");
+    graph.addEdge("Amazon", "FB");
+    graph.addEdge("Google", "MS");
+
+    expect(removeEdge(Graph2().adjacencyList, "FB", "MS")).toEqual(graph.adjacencyList);
+})
+
+// Test Case 3: //? refer to Starter -> UnweightedUndirected -> Images -> graph-3.png
+test("remove the edge between vertices 'Middle' and 'Up'", () => {
+    const vertices = ["Up", "Down", "Middle", "Left", "Right"];
+    const graph = new Graph();
+
+    generateVertices(graph, vertices);
+    graph.addEdge("Up", "Left");
+    graph.addEdge("Up", "Right");
+    graph.addEdge("Down", "Left");
+    graph.addEdge("Down", "Right");
+    graph.addEdge("Down", "Middle");
+    graph.addEdge("Middle", "Left");
+    graph.addEdge("Middle", "Right");
+
+    console.log(graph.adjacencyList);
+
+    expect(removeEdge(Graph3().adjacencyList, "Up", "Middle")).toEqual(graph.adjacencyList);
+})
+
+// Test Case 4: //? refer to Starter -> UnweightedUndirected -> Images -> graph-4.png
+test("remove the edge between vertices 2 and 0", () => {
+    const vertices = [1, 7, 2, 4, 3, 0, 9, 8];
+    const graph = new Graph();
+
+    generateVertices(graph, vertices);
+    graph.addEdge(1, 3);
+    graph.addEdge(1, 7);
+    graph.addEdge(7, 0);
+    graph.addEdge(0, 9);
+    graph.addEdge(2, 4);
+    graph.addEdge(2, 9);
+    graph.addEdge(9, 8);
+    graph.addEdge(9, 4);
+    graph.addEdge(8, 4);
+
+    console.log(graph.adjacencyList);
+
+    expect(removeEdge(Graph4().adjacencyList, 2, 0)).toEqual(graph.adjacencyList);
+})
+
