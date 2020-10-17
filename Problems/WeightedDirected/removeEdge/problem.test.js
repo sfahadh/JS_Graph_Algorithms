69451fdf83cf21485f956d78ad9f3663b6e310b9
sfahadh <sfahadh14@gmail.com>
2020-10-17 15:12:51 -0400

    added 4 test cases for remove edge problem in weighted directed

--- Problems/WeightedDirected/removeEdge/problem.test.js
+++ Problems/WeightedDirected/removeEdge/problem.test.js
@@ -68,3 +68,67 @@ test("remove the edge between vertices 'E' and 'T'", () => {
 
     expect(removeEdge(Graph4().adjacencyList, "E", "T")).toEqual(graph.adjacencyList);
 })
+
+// Test Case 5: //? refer to Starter -> WeightedDirected -> Images -> graph-5.png
+test("remove the edge between vertices 15 and 10", () => {
+    const graph = new Graph();
+    const vertices = [15, 2, 6, 3, 16, 9, 10];
+
+    generateVertices(graph, vertices);
+    graph.addEdge(2, 10, 11);
+    graph.addEdge(6, 10, -9);
+    graph.addEdge(3, 10, 18);
+    graph.addEdge(16, 10, -1);
+    graph.addEdge(9, 10, 22);
+
+    expect(removeEdge(Graph5().adjacencyList, 15, 10)).toEqual(graph.adjacencyList);
+})
+
+// Test Case 6: //? refer to Starter -> WeightedDirected -> Images -> graph-6.png
+test("remove the edge between vertices 33 and 1", () => {
+    const graph = new Graph();
+    const vertices = [18, 52, 49, 31, 13, 1, 5, 33];
+
+    generateVertices(graph, vertices);
+    graph.addEdge(18, 52, 19);
+    graph.addEdge(52, 13, 23);
+    graph.addEdge(52, 1, 8);
+    graph.addEdge(49, 18, 18);
+    graph.addEdge(49, 31, 7);
+    graph.addEdge(49, 5, 31);
+    graph.addEdge(31, 18, 3);
+    graph.addEdge(31, 52, 16);
+    graph.addEdge(31, 13, 2);
+    graph.addEdge(13, 1, 51);
+    graph.addEdge(13, 33, 8);
+    graph.addEdge(5, 31, 22);
+    graph.addEdge(5, 13, 4);
+    graph.addEdge(5, 33, 1);
+
+    expect(removeEdge(Graph6().adjacencyList, 33, 1)).toEqual(graph.adjacencyList);
+})
+
+// Test Case 7: //? refer to Starter -> WeightedDirected -> Images -> graph-7.png
+test("remove the edge between vertices 0 and 1", () => {
+    const graph = new Graph();
+    const vertices = [0, 1, 2, 3, 4, 5, 6];
+
+    generateVertices(graph, vertices);
+    graph.addEdge(0, 2, 4);
+    graph.addEdge(0, 3, 6);
+    graph.addEdge(1, 0, 4);
+    graph.addEdge(1, 4, 7);
+    graph.addEdge(4, 6, 8);
+    graph.addEdge(3, 4, 9);
+    graph.addEdge(3, 5, 10);
+    graph.addEdge(2, 0, 3);
+    graph.addEdge(2, 5, 11);
+    graph.addEdge(5, 6, 5);
+
+    expect(removeEdge(Graph7().adjacencyList, 0, 1)).toEqual(graph.adjacencyList);
+})
+
+// Test Case 8: //? refer to Starter -> WeightedDirected -> Images -> graph-8.png
+test("remove the edge between vertices 'A' and 'B'", () => {
+    expect(removeEdge(Graph8().adjacencyList, "A", "B")).toEqual(Graph8().adjacencyList);
+})
