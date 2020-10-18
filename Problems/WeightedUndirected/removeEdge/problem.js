8659897a117620754682a580c9f81ed28d40676a
sfahadh <sfahadh14@gmail.com>
2020-10-18 11:51:17 -0400

    added remove edge problem in weighted undirected

---
+++ Problems/WeightedUndirected/removeEdge/problem.js
@@ -0,0 +1,23 @@
+// ? In Visual Studo Code, install "Better Comments" to see colorful effects of comments
+
+/*
+TODO: Complete the following remove edge problem below. There are three inputs, the adjacency list 
+TODO: of the graph and the 2 vertices to remove the relationship. The goal of this problem is to 
+TODO: remove an edge between the two vertices then return the adjacency list.
+
+function WeightedUndirectedGraph() {
+    this.adjacencyList = {};
+}
+*/
+
+const removeEdge = (adjacencyList, vertix1, vertix2) => {
+
+}
+
+module.exports = removeEdge;
+
+/*
+! Test Case:
+* Sample Input: { "A": [], "B": [{ vertix: "C", weight: 10 }], "C": [{ vertix: "B", weight: 15 }] }, "B", "C"
+* Sample Output: { "A": [], "B": [], "C": [] }
+*/
