acaad7ce0c5a6181fc53d4077594b18a68a809ef
sfahadh <sfahadh14@gmail.com>
2020-10-26 15:37:57 -0400

    added solution file for remove vertix problem in unweighted undirected

--- Problems/UnweightedUndirected/removeVertix/problem.js
+++ Problems/UnweightedUndirected/removeVertix/problem.js
@@ -12,14 +12,7 @@ function UnweightedUndirectedGraph() {
 */
 
 const removeVertix = (adjacencyList, vertix) => {
-    for (let item in adjacencyList) {
-        adjacencyList[item] = adjacencyList[item].filter(v => {
-            return v !== vertix;
-        })
-    }
 
-    delete adjacencyList[vertix];
-    return adjacencyList;
 }
 
 module.exports = removeVertix;
---
+++ Problems/UnweightedUndirected/removeVertix/solution.js
@@ -0,0 +1,10 @@
+const removeVertix = (adjacencyList, vertix) => {
+    for (let item in adjacencyList) {
+        adjacencyList[item] = adjacencyList[item].filter(v => {
+            return v !== vertix;
+        })
+    }
+
+    delete adjacencyList[vertix];
+    return adjacencyList;
+}
