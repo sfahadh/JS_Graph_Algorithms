5bad0e9b8d6ee8dc8388fe39172af8430d13f742
sfahadh <sfahadh14@gmail.com>
2020-10-26 14:25:23 -0400

    added 4 more test cases for remove vertix problem in unweighted directed

--- Problems/UnweightedDirected/removeVertix/problem.test.js
+++ Problems/UnweightedDirected/removeVertix/problem.test.js
@@ -116,3 +116,85 @@ test("remove the vertix 21 and all its relationships", () => {
 
     expect(removeVertix(Graph8().adjacencyList, 21)).toEqual(graph.adjacencyList);
 })
+
+// Test Case 9: //? refer to Starter -> UnweightedDirected -> Images -> graph-9.png
+test("remove the vertix 'Thor' and all its relationships", () => {
+    const vertices = ["Thanos", "Loki", "Hulk"];
+    const graph = new Graph();
+
+    generateVertices(graph, vertices);
+    graph.addEdge("Thanos", "Loki");
+    graph.addEdge("Thanos", "Hulk");
+    graph.addEdge("Hulk", "Loki");
+    graph.addEdge("Loki", "Loki");
+    graph.addEdge("Loki", "Hulk");
+
+    expect(removeVertix(Graph9().adjacencyList, 'Thor')).toEqual(graph.adjacencyList);
+})
+
+// Test Case 10: //? refer to Starter -> UnweightedDirected -> Images -> graph-10.png
+test("remove the vertix 5 and all its relationships", () => {
+    const vertices = [-26, 12, -3, 21, 19, -9, 1, 7, -16, 6];
+    const graph = new Graph();
+
+    generateVertices(graph, vertices);
+    graph.addEdge(-26, -3);
+    graph.addEdge(-26, 19);
+    graph.addEdge(12, -9);
+    graph.addEdge(12, 1);
+    graph.addEdge(12, 7);
+    graph.addEdge(-3, 1);
+    graph.addEdge(21, 7);
+    graph.addEdge(19, 6);
+    graph.addEdge(1, -16);
+    graph.addEdge(1, 6);
+
+    expect(removeVertix(Graph10().adjacencyList, 5)).toEqual(graph.adjacencyList);
+})
+
+// Test Case 11: //? refer to Starter -> UnweightedDirected -> Images -> graph-11.png
+test("remove the vertix 7 and all its relationships", () => {
+    const vertices = [1, 4, 2, 5, 3, 6, 8];
+    const graph = new Graph();
+
+    generateVertices(graph, vertices);
+    graph.addEdge(1, 2);
+    graph.addEdge(4, 2);
+    graph.addEdge(4, 5);
+    graph.addEdge(2, 3);
+    graph.addEdge(2, 6);
+    graph.addEdge(2, 8);
+    graph.addEdge(5, 6);
+
+    expect(removeVertix(Graph11().adjacencyList, 7)).toEqual(graph.adjacencyList);
+})
+
+// Test Case 12: //? refer to Starter -> UnweightedDirected -> Images -> graph-12.png
+test("remove the vertix 22 and all its relationships", () => {
+    const vertices = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21];
+    const graph = new Graph();
+
+    generateVertices(graph, vertices);
+    graph.addEdge(1, 2);
+    graph.addEdge(2, 3);
+    graph.addEdge(3, 4);
+    graph.addEdge(4, 5);
+    graph.addEdge(5, 6);
+    graph.addEdge(6, 7);
+    graph.addEdge(7, 8);
+    graph.addEdge(8, 9);
+    graph.addEdge(9, 10);
+    graph.addEdge(10, 11);
+    graph.addEdge(11, 12);
+    graph.addEdge(12, 13);
+    graph.addEdge(13, 14);
+    graph.addEdge(14, 15);
+    graph.addEdge(15, 16);
+    graph.addEdge(16, 17);
+    graph.addEdge(17, 18);
+    graph.addEdge(18, 19);
+    graph.addEdge(19, 20);
+    graph.addEdge(20, 21);
+
+    expect(removeVertix(Graph12().adjacencyList, 22)).toEqual(graph.adjacencyList);
+})
