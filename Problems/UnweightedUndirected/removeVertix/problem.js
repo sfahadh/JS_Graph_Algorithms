482008f0b1c2ac84629574d9fbe0f460236b809b
sfahadh <sfahadh14@gmail.com>
2020-10-26 14:35:44 -0400

    added remove vertix problem in unweighted undirected

---
+++ Problems/UnweightedUndirected/removeVertix/problem.js
@@ -0,0 +1,24 @@
+// ? In Visual Studo Code, install "Better Comments" to see colorful effects of comments
+
+/*
+TODO: Complete the following remove vertix problem below. There are two inputs, the adjacency list 
+TODO: of the graph and the vertix that is to be removed from the adjacency list. The goal of this 
+TODO: problem is to completely remove the vertix including all its relationships with other vertices 
+TODO: and then return the adjacency list.
+
+function UnweightedUndirectedGraph() {
+    this.adjacencyList = {};
+}
+*/
+
+const removeVertix = (adjacencyList, vertix) => {
+
+}
+
+module.exports = removeVertix;
+
+/*
+! Test Case:
+* Sample Input: { "A": ["C"], "B": ["C"], "C": ["B", "A"] }, "B"
+* Sample Output: { "A": ["C"], "C": ["A"] }
+*/
