520177b2a17ef17032bfe9e89e858b4ed60a8cbe
sfahadh <sfahadh14@gmail.com>
2020-10-26 13:59:08 -0400

    added 4 test cases for remove vertix problem in unweighted directed

--- Problems/UnweightedDirected/removeVertix/problem.test.js
+++ Problems/UnweightedDirected/removeVertix/problem.test.js
@@ -1,4 +1,5 @@
 const removeVertix = require("./problem");
+const generateVertices = require("../../../Starter/generateVertices");
 const Graph = require("../../../Starter/UnweightedDirected/UnweightedDirectedGraph");
 const Graph1 = require("../../../Starter/UnweightedDirected/Structure/graph-1");
 const Graph2 = require("../../../Starter/UnweightedDirected/Structure/graph-2");
@@ -15,9 +16,36 @@ const Graph12 = require("../../../Starter/UnweightedDirected/Structure/graph-12"
 
 // Test Case 1: //? refer to Starter -> UnweightedDirected -> Images -> graph-1.png
 test("remove the vertix 'Yang' and all its relationships", () => {
-    const result = new Graph();
-    result.addVertix("Yin");
-    expect(removeVertix(graph1().adjacencyList, "Yang")).toEqual(result.adjacencyList);
+    const graph = new Graph();
+    graph.addVertix("Yin");
+    expect(removeVertix(Graph1().adjacencyList, "Yang")).toEqual(graph.adjacencyList);
 })
 
+// Test Case 2: //? refer to Starter -> UnweightedDirected -> Images -> graph-2.png
+test("remove the vertix 2 and all its relationships", () => {
+    const graph = new Graph();
+    graph.addVertix(1);
+    graph.addVertix(3);
+    expect(removeVertix(Graph2().adjacencyList, 2)).toEqual(graph.adjacencyList);
+})
 
+// Test Case 3: //? refer to Starter -> UnweightedDirected -> Images -> graph-3.png
+test("remove the vertix -2 and all its relationships", () => {
+    const graph = new Graph();
+    graph.addVertix(-1);
+    graph.addVertix(-3);
+    graph.addEdge(-1, -3);
+    expect(removeVertix(Graph3().adjacencyList, -2)).toEqual(graph.adjacencyList);
+})
+
+// Test Case 4: //? refer to Starter -> UnweightedDirected -> Images -> graph-4.png
+test("remove the vertix 'T' and all its relationships", () => {
+    const vertices = ["S", "D", "K"];
+    const graph = new Graph();
+
+    generateVertices(graph, vertices);
+    graph.addEdge("D", "S");
+    graph.addEdge("D", "K");
+
+    expect(removeVertix(Graph4().adjacencyList, 'T')).toEqual(graph.adjacencyList);
+})
