a951f17807061e84696d08e18c328b0f9bfc555d
sfahadh <sfahadh14@gmail.com>
2020-10-28 10:45:51 -0400

    added 4 test cases for remove vertix problem in weighted undirected

--- Problems/WeightedUndirected/removeVertix/problem.test.js
+++ Problems/WeightedUndirected/removeVertix/problem.test.js
@@ -15,8 +15,52 @@ const Graph11 = require("../../../Starter/WeightedUndirected/Structure/graph-11"
 const Graph12 = require("../../../Starter/WeightedUndirected/Structure/graph-12");
 
 // Test Case 1: //? refer to Starter -> WeightedUndirected -> Images -> graph-1.png
-test("remove the vertix 'Yang' and all its relationships", () => {
+test("remove the vertix 'Ares' and all its relationships", () => {
+    const vertices = ["Hades", "Zeus"];
     const graph = new Graph();
-    graph.addVertix("Yin");
-    expect(removeVertix(Graph1().adjacencyList, "Yang")).toEqual(graph.adjacencyList);
+
+    generateVertices(graph, vertices);
+    graph.addEdge("Hades", "Zeus", 99);
+
+    expect(removeVertix(Graph1().adjacencyList, "Ares")).toEqual(graph.adjacencyList);
+})
+
+// Test Case 2: //? refer to Starter -> WeightedUndirected -> Images -> graph-2.png
+test("remove the vertix 'Down' and all its relationships", () => {
+    const vertices = ["Up", "Left", "Right"];
+    const graph = new Graph();
+
+    generateVertices(graph, vertices);
+    graph.addEdge("Left", "Right", 21);
+
+    expect(removeVertix(Graph2().adjacencyList, "Down")).toEqual(graph.adjacencyList);
+})
+
+// Test Case 3: //? refer to Starter -> WeightedUndirected -> Images -> graph-3.png
+test("remove the vertix 'T' and all its relationships", () => {
+    const vertices = ["H", "E", "B", "O", "X"];
+    const graph = new Graph();
+
+    generateVertices(graph, vertices);
+    graph.addEdge("B", "E", 9);
+    graph.addEdge("B", "O", 15);
+    graph.addEdge("B", "X", 14);
+    graph.addEdge("B", "H", 23);
+    graph.addEdge("H", "E", 12);
+    graph.addEdge("X", "O", 7);
+
+    expect(removeVertix(Graph3().adjacencyList, "T")).toEqual(graph.adjacencyList);
+})
+
+// Test Case 4: //? refer to Starter -> WeightedUndirected -> Images -> graph-4.png
+test("remove the vertix 0 and all its relationships", () => {
+    const vertices = [12, 6, 8, -3];
+    const graph = new Graph();
+
+    generateVertices(graph, vertices);
+    graph.addEdge(12, 6, 3);
+    graph.addEdge(6, -3, 2);
+    graph.addEdge(-3, 8, 4);
+
+    expect(removeVertix(Graph4().adjacencyList, 0)).toEqual(graph.adjacencyList);
 })
